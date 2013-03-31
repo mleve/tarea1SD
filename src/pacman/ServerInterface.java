b1d838903d5ea57d7b95a88ab321dd7dd15f5e26
Luis Benitez <lbenitez000@gmail.com>
2013-03-31 03:35:19 -0400

    leer el readme

---
+++ src/pacman/ServerInterface.java
@@ -0,0 +1,13 @@
+package pacman;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServerInterface extends Remote{
+	public int registerPlayer() throws RemoteException;
+	public void registerReady(int playerId) throws RemoteException;
+	public boolean started(int playerId) throws RemoteException;
+	public void registerPosition(int playerId, int x, int y) throws RemoteException;
+	public int[][] getInfo() throws RemoteException;
+
+}
